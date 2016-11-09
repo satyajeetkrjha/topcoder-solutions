05dff89b471c2f10be4c566f59d815f21495b0f0
kolaverdi <coderboysatya@gmail.com>
2016-11-09 09:00:05 +0530

    solutions

---
+++ srm498div2250.cpp
@@ -0,0 +1,204 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+int sum;
+int steps;
+class AdditionGame {
+public:
+	int getMaximumPoints(int, int, int, int);
+};
+bool executed=false;
+int rec(int a,int b,int c,int rep){
+	  int z=max(a,max(b,c));
+	 if(z==a) && executed==false{
+	 sum+=z
+	 executed=true;
+	 rec(a-1,b,c,rep-1);
+		
+		
+	}
+
+int AdditionGame::getMaximumPoints(int A, int B, int C, int N) {
+	sum=0;
+	//int steps=1;
+	int ans=rec(A,B,C,N);
+	return ans;
+	
+}
+
+//<%:testing-code%>
+//Powered by [KawigiEdit] 2.0!
+//<%:start-tests%>
+double test0() {
+	int p0 = 3;
+	int p1 = 4;
+	int p2 = 5;
+	int p3 = 3;
+	AdditionGame * obj = new AdditionGame();
+	clock_t start = clock();
+	int my_answer = obj->getMaximumPoints(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 13;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 1;
+	int p1 = 1;
+	int p2 = 1;
+	int p3 = 8;
+	AdditionGame * obj = new AdditionGame();
+	clock_t start = clock();
+	int my_answer = obj->getMaximumPoints(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 3;
+	int p1 = 5;
+	int p2 = 48;
+	int p3 = 40;
+	AdditionGame * obj = new AdditionGame();
+	clock_t start = clock();
+	int my_answer = obj->getMaximumPoints(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 1140;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 36;
+	int p1 = 36;
+	int p2 = 36;
+	int p3 = 13;
+	AdditionGame * obj = new AdditionGame();
+	clock_t start = clock();
+	int my_answer = obj->getMaximumPoints(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 446;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int p0 = 8;
+	int p1 = 2;
+	int p2 = 6;
+	int p3 = 13;
+	AdditionGame * obj = new AdditionGame();
+	clock_t start = clock();
+	int my_answer = obj->getMaximumPoints(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 57;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+//<%:end-tests%>
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+/*	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;*/
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
