45327f24f8a9073b2450bf6c0afc109f2d5c52d6
kolaverdi <coderboysatya@gmail.com>
2016-11-15 21:20:55 +0530

    solved

---
+++ srm439div2's500.cpp
@@ -0,0 +1,134 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class PouringWater {
+public:
+	int getMinBottles(int, int);
+};
+/*1 1 1 1 1 1 1 =7
+   2 2 2 1
+    4 2 1 i.e 0111 so minimum of three*/
+/* 1 1 1 1 1 1 1 1 
+    2   2   2  2 
+	  4      4
+	      8  i.e 1000 so minimum of one */    
+    
+    
+int PouringWater::getMinBottles(int N, int K) {
+	int n=N;
+	while(__builtin_popcount(n) > K){
+		n++;
+	}
+	return n-N;// 
+}
+//<%:start-tests%>
+double test0() {
+	int p0 = 3;
+	int p1 = 1;
+	PouringWater * obj = new PouringWater();
+	clock_t start = clock();
+	int my_answer = obj->getMinBottles(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 13;
+	int p1 = 2;
+	PouringWater * obj = new PouringWater();
+	clock_t start = clock();
+	int my_answer = obj->getMinBottles(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 1000000;
+	int p1 = 5;
+	PouringWater * obj = new PouringWater();
+	clock_t start = clock();
+	int my_answer = obj->getMinBottles(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 15808;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+//<%:end-tests%>
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
